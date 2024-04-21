4448ade876af3eb767a7d024c82438571c3dc12f
Shuyang Sun <shuyangsun10@gmail.com>
2024-04-20 23:11:11 -0400

    Distill visualization working.g

--- distill.py
+++ distill.py
@@ -69,12 +69,13 @@ if __name__ == "__main__":
         im /= 255  # 0 - 255 to 0.0 - 1.0
         if len(im.shape) == 3:
             im = im[None]  # expand for batch dim
+
         pred_coco = t_coco(im, augment=False, visualize=False)[0]
         pred_face = t_face(im, augment=False, visualize=False)[0][1]
-        pred_coco_filtered = torch.concatenate(
+
+        pred_coco_filtered = torch.cat(
             [
-                pred_coco[:, 0:4, :],  # bbox
-                pred_coco[:, 5:9, :],  # person,bicycle,car,motorcycle
+                pred_coco[:, 0:9, :],  # bbox,person,bicycle,car,motorcycle
                 pred_coco[:, 10:11, :],  # bus
                 pred_coco[:, 12:13, :],  # truck
                 pred_coco[:, 21:22, :],  # dog
@@ -83,7 +84,8 @@ if __name__ == "__main__":
             ],
             dim=1,
         )
-        pred = torch.concatenate([pred_face, pred_coco_filtered], dim=1)
+
+        pred = torch.concatenate([pred_face, pred_coco_filtered[:, 4:, :]], dim=1)
 
         # If any cls confidence is higher than face, replace face bbox with coco bbox.
         should_override = (
@@ -94,5 +96,45 @@ if __name__ == "__main__":
         )
         bbox_override_mask = should_override.repeat(1, 4, 1)
         pred[:, :4, :][bbox_override_mask] = pred_coco[:, :4, :][bbox_override_mask]
-        print(pred.shape)
+
+        # TESTING CODE: Process predictions
+        from pathlib import Path
+        from utils.general import scale_boxes, cv2, non_max_suppression
+        from utils.plots import Annotator, colors
+
+        pred = non_max_suppression(pred, 0.25, 0.45, None, False, max_det=1000)
+        print(pred[0].shape)
+
+        for i, det in enumerate(pred):  # per image
+            p, im0, frame = path, im0s.copy(), getattr(dataset, "frame", 0)
+
+            p = Path(p)  # to Path
+            save_path = "/home/ssun/Desktop/distill_test.jpg"
+            s += "%gx%g " % im.shape[2:]  # print string
+            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+            imc = im0
+            names = t_coco.names
+            annotator = Annotator(im0, line_width=3, example=str(names))
+            if len(det):
+                # Rescale boxes from img_size to im0 size
+                det[:, :4] = scale_boxes(im.shape[2:], det[:, :4], im0.shape).round()
+
+                # Print results
+                for c in det[:, 5].unique():
+                    n = (det[:, 5] == c).sum()  # detections per class
+                    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+
+                # Write results
+                for *xyxy, conf, cls in reversed(det):
+
+                    c = int(cls)  # integer class
+                    label = f"{names[c]} {conf:.2f}"
+                    annotator.box_label(xyxy, label, color=colors(c, True))
+
+            # Stream results
+            im0 = annotator.result()
+
+            # Save results (image with detections)
+            cv2.imwrite(save_path, im0)
+
         exit()
